d42c85fd82820779b400928ffee66b765b8f4327
rania-ibrhem30 <raniamansour967@gmail.com>
2023-10-13 15:56:43 +0300

    unvi

---
+++ api/index.js
@@ -0,0 +1,3 @@
+const server = require('../dist/PostYourBiz7/server/main');
+
+module.exports = server.app();
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,9 @@ import { RouterModule, Routes } from '@angular/router';
 const routes: Routes = [{ path: 'listing', loadChildren: () => import('./listing/listing.module').then(m => m.ListingModule) }, { path: 'posting', loadChildren: () => import('./posting/posting.module').then(m => m.PostingModule) }, { path: 'auth', loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule) }, { path: 'shared', loadChildren: () => import('./shared/shared.module').then(m => m.SharedModule) }, { path: 'buysell', loadChildren: () => import('./buysell/buysell.module').then(m => m.BuysellModule) }];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {
+    initialNavigation: 'enabledBlocking'
+})],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
